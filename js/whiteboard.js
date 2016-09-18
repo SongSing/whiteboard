8fa39c7e430e8b48c410897b5fa916d82a1267a3
SongSing <songsingok@gmail.com>
2016-09-17 19:14:01 -0500

    socket.io overhaul

--- js/whiteboard.js
+++ js/whiteboard.js
@@ -1,239 +1,296 @@
-var mouseDown = false;
-var lastPos = undefined;
-var socket = new Socket("68.53.99.168", "5080");
-var canvas;
+var drawcanvas, canvas, socket;
+var size = 5;
+var minSize = 1;
+var maxSize = 200;
+var color = "#000000";
+var state = "draw";
 var receivedBoard = false;
-var mouseCounter = -1;
-var mouseBuffer = 1; // increase to reduce strain on server
+var resw = 1920 / 2;
+var resh = 1080 / 2;
+var skip = 1;
+var scounter = 0;
+var moved = false;
+var bgdata = "";
 
-function fmouseDown()
-{
-    mouseDown = true;
+function el(id) {
+    return document.getElementById(id);
 }
 
-function fmouseUp()
-{
-    mouseDown = false;
-    lastPos = undefined;
-}
+function noop() {}
 
-function fmouseLeave()
-{
-    mouseDown = false;
-    lastPos = undefined;
-}
+function init() {
+    drawcanvas = new Canvas(el("drawcanvas"));
 
-function init()
-{
-    canvas = $("canvas");
+    canvas = new Canvas(el("canvas"));
+    canvas.setMouseDown(mouseDown);
+    canvas.setMouseUp(mouseUp);
+    canvas.setMouseMove(mouseMoved);
 
-    canvas.get(0).getContext("2d").fillStyle = "white";
-    canvas.get(0).getContext("2d").fillRect(0, 0, 800, 600); 
-    
-    canvas.mousemove(mouseMoved);
-    
-    canvas.mousedown(function()
-    {
-        fmouseDown();
-    });
-    
-    canvas.mouseup(function()
-    {
-        fmouseUp();
-    });
-    
-    canvas.mouseleave(function()
-    {
-        fmouseLeave();
-    });
+    canvas.resize(resw, resh);
+    drawcanvas.resize(resw, resh);
 
-    $(canvas).get(0).addEventListener("touchstart", fmouseDown, false);
-    $(canvas).get(0).addEventListener("touchend", fmouseUp, false);
-    $(canvas).get(0).addEventListener("touchcancel", fmouseLeave, false);
-    $(canvas).get(0).addEventListener("touchmove", mouseMoved, false);
-    
-    socket.onMessage = handleMessage;
-    socket.onClose = function()
-    {
-        alert("Connection to the server lost.");  
-    };
-    socket.connect();
-    
-    $("#clear").click(clearBoard);
-    $("#save").click(function()
-    {
-        window.open(canvas.get(0).toDataURL(), "_blank");
-    });
-    
-    setInterval(function()
-    {
-        if (socket.socket.readyState === 1)
+    drawcanvas.setLineCap("round");
+    drawcanvas.setLineJoin("round");
+
+    socket = io.connect("http://68.53.99.168:5080");
+    socket.on("connect", connected);
+    socket.on("disconnect", disconnected);
+    socket.on("draw", drawBoard);
+    socket.on("clear", clearBoard);
+    socket.on("requestboard", sendBoard);
+    socket.on("board", receiveBoard);
+    socket.on("users", displayUsers);
+    socket.on("chat", receiveChat);
+    socket.on("bg", receiveBackground);
+
+    el("showchat").onclick = toggleChat;
+    el("save").onclick = saveBoard;
+    el("pickcolor").onclick = pickColor;
+    el("picksize").onclick = pickSize;
+    el("clear").onclick = sendClearBoard;
+    el("sizeok").onclick = sizeOk;
+    el("colorok").onclick = colorOk;
+    el("setbg").onclick = pickBackground;
+    el("clearbg").onclick = clearBackground;
+    el("filepicker").onchange = backgroundPicked;
+    el("sendMessage").onclick = sendChat;
+    el("chatMessage").onkeypress = function(e) {
+        if (e.which === 13)
         {
-            socket.sendCommand("ping", "");
+            sendChat();
         }
-    }, 60000);
-    
-    window.onbeforeunload = function()
-    {
-        sendBoard(); 
     };
-    
-    $("#sendMessage").click(sendMessage);
-    
-    $("#chatMessage").keypress(function(e)
-	{
-		if (e.which === 13)
-		{
-			sendMessage();
-		}
-	});
+
+    el("size").value = size;
+    el("color").value = color;
+
+    window.onresize = resized;
+    resized();
 }
 
-function clearBoard()
-{
-    socket.sendCommand("clear", "");
-}
+function mouseMoved(x, y, mouseDown, px, py, e) {
+    e.preventDefault();
+    moved = true;
 
-function sendMessage()
-{
-    if ($("#chatMessage").get(0).value.length > 0)
-    {    
-        socket.sendCommand("chat", $("#chatMessage").get(0).value);
-        $("#chatMessage").get(0).value = "";
-    }
-}
+    if (state === "draw") {
+        canvas.clearRect(px - size / 2 - 1, py - size / 2 - 1, size + 2, size + 2);
+        scounter++;
+        scounter %= skip;
 
-function handleMessage(msg)
-{
-    var data = Socket.unpack(msg);
-    
-    if (data.command === "draw")
-    {
-        var p = JSON.parse(data.data);
-        
-        var color = p.color;
-        var c = canvas.get(0).getContext("2d");
-        c.strokeStyle = color;
-        c.fillStyle = color;
-        c.lineWidth = p.size;
-
-        c.beginPath();
-        c.moveTo(p.p1.x, p.p1.y);
-        c.lineTo(p.p2.x, p.p2.y);
-        c.stroke();
-        c.beginPath();
-        c.arc(p.p2.x, p.p2.y, p.size / 2, 0, 2 * Math.PI);
-        c.fill();
-    }
-    else if (data.command === "clear")
-    {
-        var c = canvas.get(0);
-        c.getContext("2d").fillStyle = "white";
-        c.getContext("2d").fillRect(0, 0, 800, 600);   
-    }
-    else if (data.command === "requestboard")
-    {
-        sendBoard();
-    }
-    else if (data.command === "board")
-    {
-        var img = new Image;
-        
-        if (data.data === "")
-        {
-            receivedBoard = true;   
+        if (receivedBoard && mouseDown && e.button === 0) {
+            if (/*scounter === 0*/true) {
+                socket.emit("draw", { x1: x, x2: px, y1: y, y2: py, size: size, color: color });
+            } else {
+                canvas.pastPos = { x: px, y: py };
+                return false;
+            }
         }
-        
-        img.onload = function()
-        {
-            receivedBoard = true;
-            var c = canvas.get(0).getContext("2d");
-            c.drawImage(this, 0, 0);
+
+        canvas.drawCircleInSquare(x - size / 2, y - size / 2, size, color, 1);
+    }
+}
+
+function mouseDown(x, y) {
+    moved = false;
+}
+
+function mouseUp(x, y, px ,py) {
+    canvas.clearRect(px - size / 2 - 1, py - size / 2 - 1, size + 2, size + 2);
+    if (receivedBoard && !moved) {
+        socket.emit("draw", { x1: x, x2: px, y1: y, y2: py, size: size, color:color });
+    }
+}
+
+function connected() {
+    console.log("connected");
+}
+
+function disconnected() {
+    alert("you have been disconnected from the server :~P");
+}
+
+function drawBoard(data) {
+    drawcanvas.drawLine(data.x1, data.y1, data.x2, data.y2, data.color, data.size);
+}
+
+function clearBoard() {
+    drawcanvas.clear();
+}
+
+function sendBoard() {
+    if (receivedBoard) socket.emit("board", drawcanvas.toDataURL());
+}
+
+function receiveBoard(data) {
+    data = typeof(data) === "string" ? data : new TextDecoder("utf8").decode(data);
+    drawcanvas.drawDataURL(data, 0, 0, resw, resh, function() {
+        receivedBoard = true;
+
+        window.onbeforeunload = function() {
+            sendBoard(); 
         };
-        
-        img.src = data.data;
-    }
-    else if (data.command === "users")
-    {
-        $("#usersNumber").html(data.data);   
-    }
-    else if (data.command === "chat")
-    {
-        var c = document.createElement("div");
-        c.className = "chatItem";
-        $(c).text(data.data);
-        c.innerHTML = "<span style='color:#666666; font-family:Courier New'>" + timestamp() + "</span>&nbsp;" + c.innerHTML;
-        $("#chatMessageContainer").append(c);
-        var w = $("#chatMessageContainer").get(0);
-        w.scrollTop = w.scrollHeight;
+    });
+}
+
+function displayUsers(users) {
+    el("users").textContent = users;
+}
+
+function sendChat() {
+    var $i = el("chatMessage");
+    var c = $i.value.trim();
+
+    if (c.length > 0) {
+        socket.emit("chat", c);
+        $i.value = "";
     }
 }
 
-function timestamp()
-{
-	var time = new Date();
-	var h = time.getHours();
-	var m = time.getMinutes();
-	var s = time.getSeconds();
-	var d = time.getDate();
-	var mo = parseInt(time.getMonth() + 1);
-	var y = time.getFullYear();
-	var date = d + "-" + mo + "-" + y;
-	m = (m < 10 ? "0" + m : m);
-	s = (s < 10 ? "0" + s : s);
-	var timestamp = "(" + (h < 10 ? "0" + h : h) + ":" + m + ":" + s + ")";
-	return timestamp;
+function receiveChat(data) {
+    var $container = el("chatMessageContainer");
+    var $c = document.createElement("div");
+    $c.className = "chatItem";
+    $c.textContent = data;
+    $c.innerHTML = "<span style='color:#666666; font-family:Courier New'>" + timestamp() + "</span>&nbsp;" + $c.innerHTML;
+    $container.appendChild($c);
+    $container.scrollTop = $container.scrollHeight;
 }
 
-function sendBoard()
-{
-    if (receivedBoard)
-        socket.sendCommand("board", canvas.get(0).toDataURL());   
+function pickBackground() {
+    el("filepicker").click();
 }
 
-function mouseMoved(e)
-{
-    if (!mouseDown)
-        return;
-    
-    mouseCounter++;
-    mouseCounter %= mouseBuffer;
-    
-    if (mouseCounter !== 0)
-        return;
-    
-    var p = pos(e, "#canvas");
-    
-    if (lastPos === undefined)
-    {
-        lastPos = p;
-        return;
-    }
-    
-    var c = $("#color").get(0).value;
-    var n = parseInt($("#size").get(0).value);
-    
-    var toSend = { "p1": lastPos, "p2": p, "color": c, "size": n };
-    lastPos = p;
-    
-    socket.sendCommand("draw", JSON.stringify(toSend));
+function backgroundPicked() {
+    var file = this.files[0];
+    var mb = file.size / 1024 / 1024;
+
+    var shrinkage = mb * 2 + 1;
+    console.log(shrinkage);
+
+    Canvas.fileToImage(file, function(img) {
+        var durl = Canvas.imageToDataURL(img, shrinkage);
+        socket.emit("bg", durl);
+    });
 }
 
-function pos(e, el)
-{
-	var x = e.clientX;
-	var y = e.clientY;
-
-    if (e.changedTouches)
-    {
-        x = e.changedTouches[0].clientX;
-        y = e.changedTouches[0].clientY;
+function receiveBackground(data) {
+    if (data === "") {
+        el("drawcanvas").style["background-image"] = "";
+    } else {
+        el("drawcanvas").style["background-image"] = "url(" + data + ")";
     }
 
-	var d = $(el).get(0);
-	
-    x -= d.offsetLeft;
-    y -= d.offsetTop;
-	
-	return { "x": x, "y": y };
+    bgdata = data;
+}
+
+function clearBackground() {
+    socket.emit("bg", "");
+}
+
+function toggleChat() {
+    var $e = el("chat");
+    var $c = el("canvasArea");
+
+    if (getComputedStyle($e).getPropertyValue("display") === "none") {
+        $e.style.display = "block";
+        $c.style.left = getComputedStyle($e).getPropertyValue("width");
+    } else {
+        $e.style.display = "none";
+        $c.style.left = "0";
+    }
+
+    resized();
+    canvas.deepCalcPosition();
+    drawcanvas.deepCalcPosition();
+}
+
+function pickSize() {
+    state = "size";
+    el("over").style.display = "block";
+    el("sizePicker").style.display = "block";
+    canvas.clear();
+    el("canvasArea").style.cursor = "default";
+}
+
+function pickColor() {
+    state = "color";
+    el("over").style.display = "block";
+    el("colorPicker").style.display = "block";
+    canvas.clear();
+    el("canvasArea").style.cursor = "default";
+}
+
+function sizeOk() {
+    size = el("size").value;
+    el("sizePicker").style.display = "none";
+    el("over").style.display = "none";
+    state = "draw";
+    el("canvasArea").style.cursor = "none";
+}
+
+function colorOk() {
+    color = el("color").value;
+    color = color[0] === '#' ? color : "#" + color;
+    el("colorPicker").style.display = "none";
+    el("over").style.display = "none";
+    state = "draw";
+    el("canvasArea").style.cursor = "none";
+}
+
+function sendClearBoard() {
+    if (receivedBoard) {
+        socket.emit("clear");
+    }
+}
+
+function saveBoard() {
+    var c = new Canvas(document.createElement("canvas"));
+    c.resize(resw, resh);
+
+    if (bgdata !== "") { // if there is a background
+        c.drawDataURL(bgdata, 0, 0, resw, resh, function() {
+            console.log("1");
+            drawcanvas.getImage(function(img) {
+                c.drawImage(img, 0, 0);
+                window.open(c.toDataURL(), "_blank");
+            });
+
+        })
+    } else {
+        window.open(drawcanvas.toDataURL(), "_blank");
+    }
+
+}
+
+function resized() {
+    var $e = el("canvasArea");
+    var ar = $e.offsetWidth / $e.offsetHeight;
+    var car = canvas.width() / canvas.height();
+
+    if (ar < car) {
+        canvas.canvas.style.width = drawcanvas.canvas.style.width = "100%";
+        canvas.canvas.style.height = drawcanvas.canvas.style.height = "auto";
+    } else {
+        canvas.canvas.style.width = drawcanvas.canvas.style.width = "auto";
+        canvas.canvas.style.height = drawcanvas.canvas.style.height = "100%";
+    }
+}
+
+window.onload = function() {
+    init();
+};
+
+function timestamp() {
+    var time = new Date();
+    var h = time.getHours();
+    var m = time.getMinutes();
+    var s = time.getSeconds();
+    var d = time.getDate();
+    var mo = parseInt(time.getMonth() + 1);
+    var y = time.getFullYear();
+    var date = d + "-" + mo + "-" + y;
+    m = (m < 10 ? "0" + m : m);
+    s = (s < 10 ? "0" + s : s);
+    var timestamp = "(" + (h < 10 ? "0" + h : h) + ":" + m + ":" + s + ")";
+    return timestamp;
 }
---
+++ old/old new/js/whiteboard.js
@@ -0,0 +1,225 @@
+var mouseDown = false;
+var lastPos = undefined;
+var socket = new Socket("68.53.99.168", "5080");
+var canvas;
+var receivedBoard = false;
+var mouseCounter = -1;
+var mouseBuffer = 1; // increase to reduce strain on server
+
+function init()
+{
+    canvas = $("canvas");
+
+    canvas.get(0).getContext("2d").fillStyle = "white";
+    canvas.get(0).getContext("2d").fillRect(0, 0, 800, 600); 
+    
+    canvas.mousemove(mouseMoved);
+    
+    canvas.mousedown(function()
+    {
+        mouseDown = true;
+    });
+    
+    canvas.mouseup(function()
+    {
+        mouseDown = false;
+        lastPos = undefined;
+    });
+    
+    canvas.mouseleave(function()
+    {
+        mouseDown = false;
+        lastPos = undefined;
+    });
+
+    $("#canvasOver").mousemove(function(e)
+    {
+        var p = pos(e, "#canvasOver");
+        var c = $("#canvasOver").get(0).getContext("2d");
+        c.clearRect(0, 0, canvas.get(0).width, canvas.get(0).height);
+        c.strokeStyle = "black";
+        c.lineWidth = 1;
+
+        c.beginPath();
+        c.arc(p.x, p.y, $("#size").val(), 0, 2 * Math.PI, false);
+        c.stroke();
+    });
+    
+    socket.onMessage = handleMessage;
+    socket.onClose = function()
+    {
+        alert("Connection to the server lost.");  
+    };
+    socket.connect();
+    
+    $("#clear").click(clearBoard);
+    $("#save").click(function()
+    {
+        window.open(canvas.get(0).toDataURL(), "_blank");
+    });
+    
+    setInterval(function ()
+    {
+        if (socket.socket.readyState === 1)
+        {
+            socket.sendCommand("ping", "");
+        }
+    }, 60000);
+    
+    window.onbeforeunload = function()
+    {
+        sendBoard(); 
+    };
+    
+    $("#sendMessage").click(sendMessage);
+    
+    $("#chatMessage").keypress(function(e)
+	{
+		if (e.which === 13)
+		{
+			sendMessage();
+		}
+	});
+}
+
+function clearBoard()
+{
+    socket.sendCommand("clear", "");
+}
+
+function sendMessage()
+{
+    if ($("#chatMessage").get(0).value.length > 0)
+    {    
+        socket.sendCommand("chat", $("#chatMessage").get(0).value);
+        $("#chatMessage").get(0).value = "";
+    }
+}
+
+function handleMessage(msg)
+{
+    var data = Socket.unpack(msg);
+    
+    if (data.command === "draw")
+    {
+        var p = JSON.parse(data.data);
+        
+        var color = p.color;
+        var c = canvas.get(0).getContext("2d");
+        c.strokeStyle = color;
+        c.fillStyle = color;
+        c.lineWidth = p.size;
+
+        c.beginPath();
+        c.moveTo(p.p1.x, p.p1.y);
+        c.lineTo(p.p2.x, p.p2.y);
+        c.stroke();
+        c.beginPath();
+        c.arc(p.p2.x, p.p2.y, p.size / 2, 0, 2 * Math.PI);
+        c.fill();
+    }
+    else if (data.command === "clear")
+    {
+        var c = canvas.get(0);
+        c.getContext("2d").fillStyle = "white";
+        c.getContext("2d").fillRect(0, 0, 800, 600);   
+    }
+    else if (data.command === "requestboard")
+    {
+        sendBoard();
+    }
+    else if (data.command === "board")
+    {
+        var img = new Image;
+        
+        if (data.data === "")
+        {
+            receivedBoard = true;   
+        }
+        
+        img.onload = function()
+        {
+            receivedBoard = true;
+            var c = canvas.get(0).getContext("2d");
+            c.drawImage(this, 0, 0);
+        };
+        
+        img.src = data.data;
+    }
+    else if (data.command === "users")
+    {
+        $("#usersNumber").html(data.data);   
+    }
+    else if (data.command === "chat")
+    {
+        var c = document.createElement("div");
+        c.className = "chatItem";
+        $(c).text(data.data);
+        c.innerHTML = "<span style='color:#666666; font-family:Courier New'>" + timestamp() + "</span>&nbsp;" + c.innerHTML;
+        $("#chatMessageContainer").append(c);
+        var w = $("#chatMessageContainer").get(0);
+        w.scrollTop = w.scrollHeight;
+    }
+}
+
+function timestamp()
+{
+	var time = new Date();
+	var h = time.getHours();
+	var m = time.getMinutes();
+	var s = time.getSeconds();
+	var d = time.getDate();
+	var mo = parseInt(time.getMonth() + 1);
+	var y = time.getFullYear();
+	var date = d + "-" + mo + "-" + y;
+	m = (m < 10 ? "0" + m : m);
+	s = (s < 10 ? "0" + s : s);
+	var timestamp = "(" + (h < 10 ? "0" + h : h) + ":" + m + ":" + s + ")";
+	return timestamp;
+}
+
+function sendBoard()
+{
+    if (receivedBoard)
+        socket.sendCommand("board", canvas.get(0).toDataURL());   
+}
+
+function mouseMoved(e)
+{
+    if (!mouseDown)
+        return;
+    
+    mouseCounter++;
+    mouseCounter %= mouseBuffer;
+    
+    if (mouseCounter !== 0)
+        return;
+    
+    var p = pos(e, "#canvas");
+    
+    if (lastPos === undefined)
+    {
+        lastPos = p;
+        return;
+    }
+    
+    var c = $("#color").get(0).value;
+    var n = parseInt($("#size").get(0).value);
+    
+    var toSend = { "p1": lastPos, "p2": p, "color": c, "size": n };
+    lastPos = p;
+    
+    socket.sendCommand("draw", JSON.stringify(toSend));
+}
+
+function pos(e, el)
+{
+	var x = e.clientX;
+	var y = e.clientY;
+	var d = $(el).get(0);
+	
+    x -= d.offsetLeft;
+    y -= d.offsetTop;
+	
+	return { "x": x, "y": y };
+}
